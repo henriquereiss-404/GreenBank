35d9e4f4157dffb3a265c5f4e624db1370211e43
Henrique Reiss <henriquereiss402@gmail.com>
2022-03-09 08:46:23 +0000

    benefit component

---
+++ src/components/Benefit/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from './style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+export default function () {
+    return (
+        <Container>
+            <FontAwesomeIcon icon={faHeart}/>
+            <div>
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit auctor luctus. Nunc at sem faucibus, hendrerit nisi quis, mattis enim.</p>
+            </div>
+        </Container>
+    )
+}
---
+++ src/components/Benefit/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.article`
+    width: 248px;
+    padding: 8px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-color: #0D1B2A;
+    color: #fff;
+
+    & svg {
+        width: 48px; height: 48px;
+        color: #0AF7A7;
+    }
+
+    & div h3 { text-align: center; margin: 24px 0 8px; }
+    & div p { text-align: justify; }
+`
